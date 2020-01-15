ea261ff621c9c71e44eed887e69317f1ea18f486
Alexander Benavides <fabenavideso@gmail.com>
2020-01-14 21:09:42 -0500

    Variables

--- Net.ps1
+++ Net.ps1
@@ -1,1 +1,6 @@
-﻿
+﻿# .Net Everywhere
+
+$a = "I am learning PowerShell"
+$a
+$a.Length
+$a.GetType()
---
+++ Variables.ps1
@@ -0,0 +1,31 @@
+﻿# All variables start with a $ sign.
+
+$myVar = "Hello World"
+
+# Printing the value
+$myvar
+
+
+# Showing the type
+$myVar.GetType()
+
+# You can re assign values
+$myVar = 5
+$myVar.GetType()
+
+# You cannot re assign values when you define the variable type
+[System.Int32]$myint = 41 # Can make strongly typed variables
+$myint
+$myint.GetType()
+
+# Assign a string value will fail
+$myint = "Will fail"
+
+# There are shortcuts for most .net types
+[int] $mynewint = 32
+$mynewint.GetType()
+
+[string] $mystring = "testing"
+$mystring.GetType()
+
+
