db78599c81e9d81fe5ececbb0f0096e7e9d06034
Alexander Benavides <fabenavideso@gmail.com>
2020-01-15 21:42:09 -0500

    adding powershell variables

---
+++ Powershell variables.ps1
@@ -0,0 +1,26 @@
+﻿# Powershell variables
+
+# False and true
+$false
+$true
+
+# Null
+$null
+
+# Current directory
+$pwd
+
+# User home directory
+$Home
+
+# Info about a users machine
+$host
+
+# Process ID
+$PID
+
+# Version of powershell
+$PSVersionTable
+
+$_ # Current Object
+Get-ChildItem | Where-Object {$_.Name -like "*.ps1"}
