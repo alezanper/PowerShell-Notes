214f3a70c273d2da4950355726cedfdd75c0b236
Alexander Benavides <fabenavideso@gmail.com>
2020-01-15 21:32:27 -0500

    adding tables

---
+++ Hash Tables.ps1
@@ -0,0 +1,29 @@
+﻿# Hash Tables
+$table = @{"one"   = "uno";
+           "two"   = "dos";
+           "three" = "tres"}
+$table # full table
+$table["one"] # returns uno
+$table."one" # returns uno 
+
+# Using variables as keys
+$one = "one"
+$table.$one # returns uno
+
+# Adding values
+$table["four"] = "cuatro"
+$table
+
+# Removing values
+$table.Remove("two")
+$table
+
+# Looking for keys
+$table.Contains("one") # True
+
+# Looking for values
+$table.ContainsValue("uno") # True
+
+# Listing keys and values
+$table.Keys
+$table.Values
