117f91847dfafc55939c949aa539263b777d996c
Alexander Benavides <fabenavideso@gmail.com>
2020-01-16 21:25:37 -0500

    Adding file

---
+++ Files.ps1
@@ -0,0 +1,21 @@
+﻿# Working with Files
+Clear-Host
+Get-ChildItem "?.txt"
+
+Get-Content "a.txt"
+$a = Get-Content "a.txt"
+
+Clear-Host
+$a
+
+# This is actually an array
+$a[0] # hello
+$a[2] # Cloud
+$a.GetType() # array
+
+# Using a loop
+Clear-Host
+for($i=0 ; $i -le $a.Count; $i++)
+{
+    
+}
