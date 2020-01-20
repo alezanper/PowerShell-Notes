7987085e7114421d49786a2bf2155b4f03093147
Alexander Benavides <fabenavideso@gmail.com>
2020-01-19 20:32:42 -0500

    Creating a VM with windows

--- Powershell devops playbook/Creating_VM.ps1
+++ Powershell devops playbook/Creating_VM.ps1
@@ -1,2 +1,26 @@
-#Create a resource group
+# Create a resource group
 New-AzResourceGroup -Name myResourceGroup -Location EastUS
+
+# Create Virtual machine
+New-AzVm `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myVM" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389
+
+# Connect to virtual machine
+Get-AzPublicIpAddress -ResourceGroupName "myResourceGroup" | Select "IpAddress"
+
+# Create a remote connection
+mstsc /v:x.x.x.x
+
+# Install web server
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Clean Up Resources
+Remove-AzResourceGroup -Name myResourceGroup
+
